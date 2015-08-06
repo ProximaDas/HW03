737cf68171b2aebac5730f6724a5afbc73aeb795
ProximaDas <prox.me@gmail.com>
2015-08-06 12:55:15 -0700

    HW03

--- HW03_ex06.py
+++ HW03_ex06.py
@@ -3,53 +3,86 @@
 # (1) Please comment your code.
 # (2) Please be thoughtful when naming your variables.
 # (3) Please remove development code before submitting.
+import math
 ################################################################################
 # Exercise 1
 # When you submit only include your final function: compare
-
-
-
-
+def compare(x,y):
+    if x > y:
+        return 1
+    elif x == y:
+        return 0
+    else:
+        return -1
 
 ################################################################################
 # Exercise 2
 # When you submit only include your final function: hypotenuse
 # Do develop incrementally. Do not share here.
-
-
-
-
+def hypotenuse(a,b):
+    # Calculate the square of the hypotenuse
+    csquared = a**2 + b**2
+    # Use math.sqrt to calculate square root
+    c = math.sqrt(csquared)
+    return c
 
 ################################################################################
 # Exercise 3
 # When you submit only include your final function: is_between
-
-
-
-
+def is_between(x,y,z):
+    if x <= y and y <= z:
+        return True
+    else:
+        return False
 
 ################################################################################
 # Exercise 6
 # When you submit only include your final function: is_palindrome
-
-
-
-
+def is_palindrome(s):
+    reverse = s[::-1]
+    if s == reverse:
+        return True
+    else:
+        return False
 
 ################################################################################
 # Exercise 7
 # When you submit only include your final function: is_power
 
+def is_power(a,b):
+    result = 'Uninitialized'
+    def check_power(quotient,b):
+        remainder = quotient % b
+        if remainder == 0:
+            quotient = quotient / b
+            if quotient == 1:
+                global result 
+                result = True
+            else:    
+                check_power(quotient,b)   
+        else:
+            result = False
+        return result
+        
+    def check_div(a,b):
+        if a % b == 0:
+            return True
+        else:
+            return False
 
-
-
+    x = a / b
+    if check_power(x,b) and check_div(a,b):
+        return True
+    else:
+        return False
+        
 
 ################################################################################
 def main():
     """Your functions will be called within this function."""
     ############################################################################
     # Use this space temporarily to call functions in development:
-    print("Hello World!")
+    # print("Hello World!")
 
 
 
@@ -59,29 +92,29 @@ def main():
 
     ############################################################################
     # Uncomment the below to test and before commiting:
-    # # Exercise 1
-    # compare(1,1)
-    # compare(1,2)
-    # compare(2,1)
-    # # Exercise 2
-    # hypotenuse(1,1)
-    # hypotenuse(3,4)
-    # hypotenuse(1.2,12)
-    # # Exercise 3
-    # is_between(1,2,3)
-    # is_between(2,1,3)
-    # is_between(3,1,2)
-    # is_between(1,1,2)
-    # # Exercise 6
-    # is_palindrome("Python")
-    # is_palindrome("evitative")
-    # is_palindrome("sememes")
-    # is_palindrome("oooooooooooo")
-    # # Exercise 7
-    # is_power(28,3)
-    # is_power(27,3)
-    # is_power(248832,12)
-    # is_power(248844,12)
+    # Exercise 1
+    print compare(1,1)
+    print compare(1,2)
+    print compare(2,1)
+    # Exercise 2
+    print hypotenuse(1,1)
+    print hypotenuse(3,4)
+    print hypotenuse(1.2,12)
+    # Exercise 3
+    print is_between(1,2,3)
+    print is_between(2,1,3)
+    print is_between(3,1,2)
+    print is_between(1,1,2)
+    # Exercise 6
+    print is_palindrome("Python")
+    print is_palindrome("evitative")
+    print is_palindrome("sememes")
+    print is_palindrome("oooooooooooo")
+    # Exercise 7
+    print is_power(28,3)
+    print is_power(27,3)
+    print is_power(248832,12)
+    print is_power(248844,12)
 
 
 if __name__ == "__main__":
--- HW03_string1.py
+++ HW03_string1.py
@@ -26,7 +26,10 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   # +++your code here+++
-  return
+  if count < 10:
+    return "Number of donuts: " + str(count)
+  else:
+    return "Number of donuts: many"
 
 
 # B. both_ends
@@ -36,8 +39,15 @@ def donuts(count):
 # is less than 2, return instead the empty string.
 def both_ends(s):
   # +++your code here+++
-  return
-
+  length = len(s)
+  if length > 2:
+    new_string = s[:2] + s[-2:]
+    return new_string
+  elif length == 2:
+    return s
+  else:
+    return ''
+    
 
 # C. fix_start
 # Given a string s, return a string
@@ -50,7 +60,9 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
   # +++your code here+++
-  return
+  char = s[0]
+  new_string = s[0] + s[1:].replace(char,'*')
+  return new_string
 
 
 # D. MixUp
@@ -62,7 +74,9 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  str1 = b[:2] + a[2:]
+  str2 = a[:2] + b[2:]
+  return str1 + ' ' + str2
 
 
 # Provided simple test() function used in main() to print
